207131e59a17d735000d52e463f69917fe65c9d8
Josh Beech <jwbeech@btinternet.com>
2017-03-09 00:11:23 +0000

    Some slight updates to the village prefab: Renamed blacksmith character to blacksmith and made hierarchy changes. Added simple player movement. Added shoulder camera and can switch between that and Overview camera.

---
+++ MotusSystemDemo/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject TopDownCamera;
+    public GameObject ShoulderCamera;
+
+    public float MovementSpeed;
+    public float TurnSpeed;
+
+    private CharacterController m_Controller;
+
+    // Use this for initialization
+    void Start()
+    {
+        ShoulderCamera.SetActive(false);
+        m_Controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            SwapCamera();
+        }
+
+        Debug.DrawLine(transform.position, transform.position + transform.forward);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float l_VerticalInput = Input.GetAxis("Vertical");
+        float l_HorizontalInput = Input.GetAxis("Horizontal");
+
+        m_Controller.SimpleMove(transform.forward * l_VerticalInput * MovementSpeed * Time.fixedDeltaTime);
+
+        transform.Rotate(transform.up * l_HorizontalInput * TurnSpeed * Time.fixedDeltaTime);
+    }
+
+    public void SwapCamera()
+    {
+        if(TopDownCamera.activeInHierarchy)
+        {
+            TopDownCamera.SetActive(false);
+            ShoulderCamera.SetActive(true);
+        }
+        else
+        {
+            ShoulderCamera.SetActive(false);
+            TopDownCamera.SetActive(true);
+        }
+    }
+}
